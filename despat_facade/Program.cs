ed66b3e27c1c19751cac3e89d551bd86220d8ad3
mjfdassen <martijn.dassen@zuyd.nl>
2024-06-11 19:57:07 +0200

    added 3 patterns

---
+++ despat_facade/Program.cs
@@ -0,0 +1,103 @@
+﻿class RobotBody
+{
+    string robotType;
+    /*
+    * To keep a count of number of robots.
+    * This operation is optional for you.
+    */
+    static int count = 0;
+    public RobotBody(string robotType)
+    {
+        this.robotType = robotType;
+    }
+    public void MakeRobotBody()
+    {
+        Console.WriteLine($"Constructing one {robotType} robot.");
+        Console.WriteLine("Robot creation finished.");
+        Console.WriteLine($"Total number of robots created at this moment = {++count}");
+    }
+    public void DestroyRobotBody()
+    {
+        if (count > 0)
+        {
+            --count;
+            Console.WriteLine("Robot's destruction process is over.");
+        }
+        else
+        {
+            Console.WriteLine("All robots are destroyed.");
+            Console.WriteLine("Color removal operation will not continue.");
+        }
+    }
+}
+
+public class RobotColor
+{
+    string color;
+    public RobotColor(string color)
+    {
+        this.color = color;
+    }
+    public void SetColor()
+    {
+        if (color == "steel")
+        {
+            Console.WriteLine($"The default color {color} is set for the robot.");
+        }
+        else
+        {
+            Console.WriteLine($"Painting the robot with your favourite {color} color.");
+        }
+        Console.WriteLine();
+    }
+    public void RemoveColor()
+    {
+        Console.WriteLine("Attempting to remove the colors from the robot.");
+    }
+}
+
+class RobotFacade
+    //opdracht bouw deze facade om naar facade waarin de robot automatisch geconstrueerd wordt.
+{
+    RobotBody robotBody;
+    RobotColor robotColor;
+    public RobotFacade(string robotType, string color = "steel")
+    {
+        robotBody = new RobotBody(robotType);
+        robotColor = new RobotColor(color);
+    }
+    public void ConstructRobot()
+    {
+        Console.WriteLine("Robot creation through facade starts...");
+        robotBody.MakeRobotBody();
+        robotColor.SetColor();
+    }
+    public void DestroyRobot()
+    {
+        Console.WriteLine("Making an attempt to destroy one robot using the facade now.");
+        robotColor.RemoveColor();
+        robotBody.DestroyRobotBody();
+    }
+}
+
+public class TestRun
+{
+    static void Main(string[] args)
+    {
+        //without facade
+        RobotBody robotBody = new RobotBody("Milano");
+        robotBody.MakeRobotBody();
+        RobotColor robotColor = new RobotColor("green");
+        robotColor.SetColor();
+        //with facade: make a green Milano
+        RobotFacade facade1 = new RobotFacade("Milano", "green");
+        facade1.ConstructRobot();
+        //with facade: make a steel-colored Robonaut
+        RobotFacade facade2 = new RobotFacade("Robonaut");
+        facade2.ConstructRobot();
+        //with facade: make a steel-colored Johnny 5
+        RobotFacade facade3 = new RobotFacade("Johnny 5");
+        facade3.ConstructRobot();
+        //Console.ReadKey();
+    }
+}
---
+++ despat_strategy/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace StrategyPattern
+{
+    public abstract class VehicleBehavior
+    {
+        public abstract void AboutMe(string vehicle);
+    }
+
+    class FloatBehavior : VehicleBehavior
+    {
+        public override void AboutMe(string vehicle)
+        {
+            Console.WriteLine($"My {vehicle} can float now.");
+        }
+    }
+  
+    class FlyBehavior : VehicleBehavior
+    {
+        public override void AboutMe(string vehicle)
+        {
+            Console.WriteLine($"My {vehicle} can fly now.");
+        }
+    }
+
+    class InitialBehavior : VehicleBehavior
+    {
+        public override void AboutMe(string vehicle)
+        {
+            Console.WriteLine($"My {vehicle} is just born.It cannot do anything special.");
+        }
+    }
+ 
+    public class Vehicle
+    {
+        VehicleBehavior behavior;
+        string vehicleType;
+        public Vehicle(string vehicleType)
+        {
+            this.vehicleType = vehicleType;
+            //Setting the initial behavior
+            this.behavior = new InitialBehavior();
+        }
+ 
+        public void SetVehicleBehavior(VehicleBehavior behavior)
+        {
+            this.behavior = behavior;
+        }
+   
+        public void DisplayAboutMe()
+        {
+            behavior.AboutMe(vehicleType);
+        }
+    }
+
+    class Client
+    {
+        static void Main(string[] args)
+            {
+                Console.WriteLine("***Strategy Pattern Demo.***\n");
+                Vehicle context = new Vehicle("Aeroplane");
+                context.DisplayAboutMe();
+                Console.WriteLine("Setting flying capability to vehicle.");
+                context.SetVehicleBehavior(new FlyBehavior());
+                context.DisplayAboutMe();
+                Console.WriteLine("Changing the vehicle behavior again.");
+                context.SetVehicleBehavior(new FloatBehavior());
+                context.DisplayAboutMe();
+                Console.ReadKey();
+        }
+    }
+}
